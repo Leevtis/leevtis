be3c7c7e5a0a29f120956652c58f9f3f4da1a2d0
Leevtis <97434856+Leevtis@users.noreply.github.com>
2022-08-22 17:56:45 +0800

    Create OERC1155.sol

---
+++ contracts/token/OERC1155.sol
@@ -0,0 +1,19 @@
+//  SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract OERC1155 is ERC1155{
+    uint256 public constant OPT = 0;
+    uint256 public constant OPK = 1;
+    uint256 public constant OPNFT = 2;
+    
+    //Add 1155 tokens to the address
+    constructor (address userAddr) ERC1155("TOKEN_1155") {
+        _mint(userAddr, OPT, 10**18, "");
+        _mint(userAddr, OPK, 10**27, "");
+        _mint(userAddr, OPNFT, 1, "");
+    }
+
+}
